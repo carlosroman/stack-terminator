dee0ea8d8eb46525c5b624a7e68c3de7f2650c71
Carlos Roman <carlos@cliche-corp.co.uk>
2018-04-29 22:36:22 +0100

    :construction: Added basic impl to clear S3 buckets from stack

--- main.go
+++ main.go
@@ -7,10 +7,64 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	cf "github.com/aws/aws-sdk-go/service/cloudformation"
 	cfAPI "github.com/aws/aws-sdk-go/service/cloudformation/cloudformationiface"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"os"
 )
+
+func deleteDeleteMarkers(ctx context.Context, svc s3iface.S3API, bucket *string, objs []*s3.DeleteMarkerEntry) error {
+	if len(objs) == 0 {
+		return nil
+	}
+
+	dels := make([]*s3.ObjectIdentifier, len(objs))
+	for i, obj := range objs {
+		log.Info("obj:", aws.StringValue(obj.Key), ",", aws.StringValue(obj.VersionId))
+		key := *obj.Key
+		versionId := *obj.VersionId
+		dels[i] = &s3.ObjectIdentifier{
+			Key:       aws.String(key),
+			VersionId: aws.String(versionId),
+		}
+	}
+
+	res, err := svc.DeleteObjectsWithContext(ctx, &s3.DeleteObjectsInput{
+		Bucket: bucket,
+		Delete: &s3.Delete{
+			Objects: dels,
+		},
+	})
+	log.Info(res)
+	return err
+}
+
+func deleteObjectVersions(ctx context.Context, svc s3iface.S3API, bucket *string, objs []*s3.ObjectVersion) error {
+	if len(objs) == 0 {
+		return nil
+	}
+
+	dels := make([]*s3.ObjectIdentifier, len(objs))
+	for i, obj := range objs {
+		log.Info("obj:", aws.StringValue(obj.Key), ",", aws.StringValue(obj.VersionId))
+		key := *obj.Key
+		versionId := *obj.VersionId
+		dels[i] = &s3.ObjectIdentifier{
+			Key:       aws.String(key),
+			VersionId: aws.String(versionId),
+		}
+	}
+
+	res, err := svc.DeleteObjectsWithContext(ctx, &s3.DeleteObjectsInput{
+		Bucket: bucket,
+		Delete: &s3.Delete{
+			Objects: dels,
+		},
+	})
+	log.Info(res)
+	return err
+}
 
 func main() {
 	log.SetFormatter(&log.TextFormatter{})
@@ -21,13 +75,105 @@ func main() {
 	fmt.Printf("hello, world\n")
 	sess := session.Must(session.NewSession())
 	cfsvc := cf.New(sess)
+	s3svc := s3.New(sess)
 	ctx := context.Background()
-	Termainte("bob", ctx, cfsvc)
+	Termainte("bob", ctx, cfsvc, s3svc)
 }
 
-func Termainte(stackName string, ctx context.Context, cfsvc cfAPI.CloudFormationAPI) error {
+func deleteS3Content(ctx context.Context, r *cf.StackResource, s3svc s3iface.S3API) error {
+	count := 0
+	log.Info("===========================")
+	log.Info(r)
+	log.Info("===========================")
 
-	res, err := cfsvc.DeleteStackWithContext(ctx, &cf.DeleteStackInput{
+	objs, err := s3svc.ListObjectVersionsWithContext(ctx, &s3.ListObjectVersionsInput{
+		Bucket:  r.PhysicalResourceId,
+		MaxKeys: aws.Int64(5),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if err := deleteObjectVersions(ctx, s3svc, r.PhysicalResourceId, objs.Versions); err != nil {
+		return err
+	}
+
+	if err := deleteDeleteMarkers(ctx, s3svc, r.PhysicalResourceId, objs.DeleteMarkers); err != nil {
+		return err
+	}
+
+	count = count + len(objs.Versions)
+	vim := aws.StringValue(objs.NextVersionIdMarker)
+	km := aws.StringValue(objs.NextKeyMarker)
+
+	for {
+		log.Info("vim:", vim, ",km:", km)
+		if (len(objs.Versions) < 1) && (len(objs.DeleteMarkers) < 1) {
+			return err
+		}
+		objs, err := s3svc.ListObjectVersionsWithContext(ctx, &s3.ListObjectVersionsInput{
+			Bucket:          r.PhysicalResourceId,
+			MaxKeys:         aws.Int64(5),
+			KeyMarker:       aws.String(km),
+			VersionIdMarker: aws.String(vim),
+		})
+		log.Info(objs)
+		if err != nil {
+			return err
+		}
+
+		if (len(objs.Versions) < 1) && (len(objs.DeleteMarkers) < 1) {
+			return err
+		}
+
+		if err := deleteObjectVersions(ctx, s3svc, r.PhysicalResourceId, objs.Versions); err != nil {
+			return err
+		}
+
+		if err := deleteDeleteMarkers(ctx, s3svc, r.PhysicalResourceId, objs.DeleteMarkers); err != nil {
+			return err
+		}
+
+		count = count + len(objs.Versions)
+
+		if len(aws.StringValue(objs.NextKeyMarker)) < 1 {
+			break
+		}
+		km = aws.StringValue(objs.NextKeyMarker)
+		vim = aws.StringValue(objs.NextVersionIdMarker)
+	}
+	log.Info("===========================")
+	log.Info(count)
+	log.Info("===========================")
+	return err
+}
+
+func Termainte(stackName string, ctx context.Context, cfsvc cfAPI.CloudFormationAPI, s3svc s3iface.S3API) error {
+
+	res, err := cfsvc.DescribeStackResourcesWithContext(ctx, &cf.DescribeStackResourcesInput{
+		StackName: aws.String("test-stack"),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	for _, r := range res.StackResources {
+
+		switch rt := *r.ResourceType; rt {
+		case "AWS::S3::Bucket":
+			err = deleteS3Content(ctx, r, s3svc)
+		default:
+			log.Info("Ignoring resource : ", rt)
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	del, err := cfsvc.DeleteStackWithContext(ctx, &cf.DeleteStackInput{
 		StackName: aws.String(stackName),
 	})
 
@@ -35,6 +181,6 @@ func Termainte(stackName string, ctx context.Context, cfsvc cfAPI.CloudFormation
 		return err
 	}
 
-	log.Info(res.GoString())
+	log.Info(del.GoString())
 	return err
 }
--- stack_terminator_test.go
+++ stack_terminator_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/request"
 	cf "github.com/aws/aws-sdk-go/service/cloudformation"
 	cfAPI "github.com/aws/aws-sdk-go/service/cloudformation/cloudformationiface"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	. "github.com/carlosroman/stack-terminator"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -25,16 +27,15 @@ var _ = Describe("StackTerminator", func() {
 		ctx := context.Background()
 
 		Context("when stack deletes succesfully", func() {
-			mockCfSvc := &mockCloudFormationClient{}
+			mockCfSvc := &mockCloudFormationClient{ctx: ctx}
+			mockS3Svc := &mockS3Client{ctx: ctx}
 
-			mockCfSvc.
-				On(
-					"DeleteStackWithContext",
-					ctx,
-					mock.AnythingOfType("*cloudformation.DeleteStackInput"),
-					[]request.Option(nil)).
-				Return(&cf.DeleteStackOutput{}, nil)
-			err := Termainte("bob", ctx, mockCfSvc)
+			mockCfSvc.givenDescribeStackResourcesWithContextReturns(nil)
+			mockS3Svc.givenListObjectVersionsWithContextReturns(nil)
+
+			mockCfSvc.givenDeleteStackWithContextReturns(nil)
+
+			err := Termainte("bob", ctx, mockCfSvc, mockS3Svc)
 
 			It("should call CloudFormation Delete", func() {
 				Expect(
@@ -60,11 +61,66 @@ var _ = Describe("StackTerminator", func() {
 type mockCloudFormationClient struct {
 	cfAPI.CloudFormationAPI
 	mock.Mock
+	ctx context.Context
 }
 
 func (m *mockCloudFormationClient) DeleteStackWithContext(ctx aws.Context, input *cf.DeleteStackInput, opts ...request.Option) (*cf.DeleteStackOutput, error) {
 	log.Info("DeleteStackWithContext called with:", input, opts)
 	args := m.Called(ctx, input, opts)
-	dso := args.Get(0).(*cf.DeleteStackOutput)
-	return dso, args.Error(1)
+	out := args.Get(0).(*cf.DeleteStackOutput)
+	return out, args.Error(1)
+}
+
+func (m *mockCloudFormationClient) givenDeleteStackWithContextReturns(err error) {
+	m.On(
+		"DeleteStackWithContext",
+		m.ctx,
+		mock.AnythingOfType("*cloudformation.DeleteStackInput"),
+		[]request.Option(nil)).
+		Return(&cf.DeleteStackOutput{}, err)
+}
+
+func (m *mockCloudFormationClient) DescribeStackResourcesWithContext(ctx aws.Context, input *cf.DescribeStackResourcesInput, opts ...request.Option) (*cf.DescribeStackResourcesOutput, error) {
+	log.Info("DescribeStackResourcesWithContext called with:", input, opts)
+	args := m.Called(ctx, input, opts)
+	out := args.Get(0).(*cf.DescribeStackResourcesOutput)
+	return out, args.Error(1)
+}
+
+func (m *mockCloudFormationClient) givenDescribeStackResourcesWithContextReturns(err error) {
+	m.On(
+		"DescribeStackResourcesWithContext",
+		m.ctx,
+		mock.AnythingOfType("*cloudformation.DescribeStackResourcesInput"),
+		[]request.Option(nil)).
+		Return(&cf.DescribeStackResourcesOutput{
+			StackResources: []*cf.StackResource{
+				{
+					ResourceType:       aws.String("AWS::S3::Bucket"),
+					PhysicalResourceId: aws.String("bob-s3-bucket"),
+				},
+			},
+		}, err)
+}
+
+type mockS3Client struct {
+	s3iface.S3API
+	mock.Mock
+	ctx context.Context
+}
+
+func (m *mockS3Client) ListObjectVersionsWithContext(ctx aws.Context, input *s3.ListObjectVersionsInput, opts ...request.Option) (*s3.ListObjectVersionsOutput, error) {
+	log.Info("ListObjectVersionsWithContext called with:", input, opts)
+	args := m.Called(ctx, input, opts)
+	out := args.Get(0).(*s3.ListObjectVersionsOutput)
+	return out, args.Error(1)
+}
+
+func (m *mockS3Client) givenListObjectVersionsWithContextReturns(err error) {
+	m.On(
+		"ListObjectVersionsWithContext",
+		m.ctx,
+		mock.AnythingOfType("*s3.ListObjectVersionsInput"),
+		[]request.Option(nil)).
+		Return(&s3.ListObjectVersionsOutput{}, err)
 }
